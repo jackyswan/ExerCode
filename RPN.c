3720ab623ead05aca658855c2ffac53de19596e4
jackyswan <jackyswan@foxmail.com>
2018-07-02 23:14:06 +0800

    添加中缀表达式转后缀的方法

--- RPN.c
+++ RPN.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-typedef char ElemtType;
+/*总结规则
+ * 1. 数字直接输出
+ * 2. 右括号或者优先级低于栈顶符号的则出栈
+ *    直到遇到左括号或者栈空将这个符号入栈*/
+typedef double ElemtType;
 
 typedef struct stack{
     ElemtType *top;
@@ -16,9 +21,15 @@ void InitStack(stack *s, int nSize)
     s->StackSize = nSize;
 }
 
-pStack Pop(pStack s, ElemtType *n)
+void Pop(pStack s, ElemtType *n)
 {
     n = s->top;
+    s->top --;
+}
+
+void Push(pStack s, ElemtType *n)
+{
+    *n = *(s->top);
     s->top --;
 }
 
@@ -27,17 +38,87 @@ int StackLen(pStack s)
     return (s->top - s->base)/sizeof(ElemtType);
 }
 
+Trans(char*)
+{
+    while(1)
+    {
+        switch(str[i])
+        {
+            case ')':
+                Pop(sqStack, )
+            case '*':
+                Pop(sqStack, )
+            default:
+                Push(sqStack,)
+        }
+    }
+}
+
 int main(void)
 {
     pStack sqStack;
     char c;
+    ElemtType d, e;
+    char str[10];
+    int i = 0;
 
     printf("请按照逆波兰算法输入表达式，数据和运算符用空格隔开:\n");
-    scanf("%c", c);
-
-    InitStack(sqStack);
+    scanf("%c", &c);
+    InitStack(sqStack, 20);
     
-    int i;
-
-
+    while(c != '#')
+    {
+        while(isdigit(c) || c == '.')
+        {
+            str[i++] = c;
+            str[i] = '\0'
+            if ( i >= 10)
+            {
+                printf("输出的数据过大\n");
+                return -1;
+            }
+            scanf("%c", &c);
+            if (c == ' ')
+            {
+                d = atof(str);
+                Push(sqStack, d);
+                i = 0;
+                break;
+            }
+        }
+        switch (c)
+        {
+        case '+':
+            Pop(sqStack, &d);
+            Pop(sqStack, &e);
+            Push(sqStack, e+d);
+            break;
+        case '-':
+            Pop(sqStack, &d);
+            Pop(sqStack, &e);
+            Push(sqStack, e-d);
+            break;
+        case '*':
+            Pop(sqStack, &d);
+            Pop(sqStack, &e);
+            Push(sqStack, e*d);
+            break;
+        case '/':
+            Pop(sqStack, &d);
+            Pop(sqStack, &e);
+            if (d != 0)
+            {
+                Push(sqStack, e/d);
+            }
+            else
+            {
+                printf("除数不能为0!!!\n");
+                return -1;
+            }
+            break;
+        }
+    }
+    Pop(sqStack, &d);
+    Printf("计算结果:%f\n", d);
+    return 0;
 }
