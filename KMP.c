25d52c915ca9d6d66ea80bfb152348368681ecb3
jackyswan <jackyswan@foxmail.com>
2018-07-04 23:52:04 +0800

    添加KMP算法

---
+++ KMP.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*失配的时候用哪个元素进行下一轮匹配
+ *用next数组来保存匹配串的特性
+ */
+
+void get_next(String T, int *next)
+{
+    int j = 0;
+    int i = 1;
+    next[1] = 0;
+
+    while ( i < T[0])
+    {
+        if ( T[i] == T[j] )
+        {
+            i ++;
+            j ++;
+            next[i] = j;
+        }
+        else
+        {
+            j = next[j];
+        }
+    }
+}
+
