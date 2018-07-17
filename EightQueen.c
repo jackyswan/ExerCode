90f076cace2efb6e06dd11bb15c45261143b4d66
jackyswan <jackyswan@foxmail.com>
2018-07-18 00:45:24 +0800

    debug

--- EightQueen.c
+++ EightQueen.c
@@ -1,28 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int EightQueen(int row, int n, int (*chess)[8] )
+int count;
+
+int JudgeDanger(int row, int pos, int n, int(*chess)[8])
 {
-    int count, i, j;
+    int i, j;
+
+    //左上方
+    for (i = row; i - 1 > 0 && pos - 1 > 0; i--)
+    {
+        if(chess[i-1][pos-1])
+            return 0;
+        pos--;
+    }
+    //正上方
+    for(i = row; i - 1 > 0; i--)
+    {
+        if(chess[i-1][pos])
+            return 0;
+    }
+    //右上方
+    for(i = row; i-1 > 0 && pos + 1< n; i--)
+    {
+        if(chess[i-1][pos+1])
+            return 0;
+        pos++;
+    }
+
+    return 1;
+}
+
+void EightQueen(int row, int n, int (*chess)[8])
+{
+    int i, j;
     int chess2[n][n];
 
-    for(i=0; i < n;)
+    if (row == n)
+    {
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < n; j++)
+            {
+                printf("%d ", chess[i][j]);
+            }
+        }
+
+        count ++;
+        printf("\n");
+        printf("这是第%d种布局\n", count);
+    }
+    else
+    {
+        for(i = 0; i < n; i++)
+        {
+            for(j = 0; j < n; j ++)
+            {
+                chess2[i][j] = chess[i][j];
+            }
+        }
+
+        for(j = 0; j < n; j++)
+        {
+            if (JudgeDanger(row, j, n, chess2))
+            {
+                chess2[row][j] = 1;
+                EightQueen(row + 1, n, chess2);
+            }
+        }
+    }
 }
 
 int main(void)
 {
     int chess[8][8], i, j;
-    int count;
 
-    for( i=0; i < 8; i++)
+    for( i = 0; i < 8; i++)
     {
-        for( j=0; j < 8; j++)
+        for( j = 0; j < 8; j++)
         {
             chess[i][j] = 0;
         }
     }
 
-    count = EightQueen(0, 8, chess);
+    EightQueen(0, 8, chess);
 
     printf("总共有多少种方案：%d\n", count);
 }
