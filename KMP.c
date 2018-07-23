3e5f1d0d047a6f1483968d95b7e98ce21b5f99e4
jackyswan <jackyswan@foxmail.com>
2018-07-23 14:54:27 +0800

    fulfill it

--- KMP.c
+++ KMP.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
-/*失配的时候用哪个元素进行下一轮匹配
+typedef struct String{
+    int num;
+    char* pStr;
+} pString*;
+/*失配的时候用T串中的哪个元素进行下一轮匹配
  *用next数组来保存匹配串的特性
  */
 
-void get_next(String T, int *next)
+void get_next(pString T, int *next)
 {
     int j = 0;
     int i = 1;
@@ -29,9 +33,51 @@ void get_next(String T, int *next)
         }
         else
         {
-            j = next[j];
+            j = next[j];// 在子串的j位置失配从next[j]位置开始适配
         }
     }
 }
 
+int Index_KMP( String S, String T)
+{
+    int i = 0;
+    int j = 1;
+    int next[255];
 
+    get_next(T, next);
+
+    while(i<S[0] && j <= T[0])
+    {
+        if (j == 0 || S[i] == T[j])
+        {
+            i ++;
+            j ++;
+        }
+        else
+        {
+            j = next[j];
+        }
+    }
+
+    if(j > T[0])
+    {
+        return i - T[0];
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main (void)
+{
+    char str[255] = " ababaaaba";
+    int i;
+    char T[5]=" aaba";
+    T[0] = 4;
+
+    str[0] = 9;
+    printf("%d\n", str[0]);
+    i = Index_KMP(str, T);
+    printf("位置在%d\n", i);
+}
